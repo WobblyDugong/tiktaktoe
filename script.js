b6bb5ae6bee5f47c7d781b5684739fcd7eaf28b1
WobblyDugong <banhidibotond@gmail.com>
2022-03-22 11:48:29 +0100

    id kattintás működik

--- script.js
+++ script.js
@@ -1,30 +1,33 @@
 window.addEventListener("load", init);
 
 function ID(elem) {
-    return document.getElementById(elem);
-  }
-  function CLASS(elem){
-      return document.getElementsByClassName(elem);
-  }
-  function $(elem){
-      return document.querySelectorAll(elem);
-  }
+  return document.getElementById(elem);
+}
+function CLASS(elem) {
+  return document.getElementsByClassName(elem);
+}
+function $(elem) {
+  return document.querySelectorAll(elem);
+}
 
-  var jatek = [];
+function init() {
+  rublikak();
+}
 
-  function init(){
-    rublikak();
-    kattint();
+function rublikak() {
+  var txt = "";
+  for (let index = 1; index <= 9; index++) {
+    txt += `<div class="rublika" id="${index}"></div>`;
   }
-
-  function rublikak(){
-    var txt = "";
-    for (let index = 1; index <= 9; index++) {
-      txt += `<div class="rublika"></div>`;
-    }
-    ID("jatekter").innerHTML = txt;
-  }
-
-  function kattint(){
+  ID("jatekter").innerHTML = txt;
+  for (let index = 1; index <= 9; index++) {
+      ID(index).addEventListener("click", valasztas)
       
   }
+}
+
+function valasztas(){
+    console.log(event.target.id);
+    
+}
+
